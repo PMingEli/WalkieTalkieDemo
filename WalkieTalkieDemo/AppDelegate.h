41456b83eac52b999018da5d7378f67566bdcb2e
cseMing <cseming@163.com>
2022-08-15 15:14:17 +0800

    Initial Commit

---
+++ WalkieTalkieDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  WalkieTalkieDemo
+//
+//  Created by 彭明均 on 2022/8/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ WalkieTalkieDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WalkieTalkieDemo
+//
+//  Created by 彭明均 on 2022/8/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
